0e8a370727a20d48f59c3878c92a065bec2d564d
katharmoi <kadirkertis@gmail.com>
2021-05-08 16:40:45 +0300

    Add E2E tests

--- app/src/androidTest/java/io/appicenter/payoneer/home/HomeActivityTest.java
+++ app/src/androidTest/java/io/appicenter/payoneer/home/HomeActivityTest.java
@@ -1,6 +1,7 @@
 package io.appicenter.payoneer.home;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
@@ -9,6 +10,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.appicenter.payoneer.IdlingResourceRule;
+import io.appicenter.payoneer.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * End-to-End tests
@@ -20,9 +27,12 @@ public class HomeActivityTest {
     @Rule
     public IdlingResourceRule rule = new IdlingResourceRule();
 
+
     @Test
-    public void addProductsToCartAndRecreateActivity_cartIsRepopulatedOnResumed() {
+    public void paymentMethods() {
         ActivityScenario.launch(HomeActivity.class);
+
+        onView(withId(R.id.payment_methods_recycler_view)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
     }
 
 
